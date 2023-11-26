365ad97ec0f73839f2bfed7dc2c1095144c9101a
MNlegion <rkreuser30@gmail.com>
2023-11-26 17:42:42 -0500

    typo update

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function () {
     getWeatherBtn.addEventListener("click", getWeather);
 });
 
-function createWeatherCard(temperature, description) {
+function createWeatherCard(temperature, description, icon) {
     // Create card container
     const card = document.createElement("div");
     card.classList.add("weather-card", "bg-gray-800", "p-4", "rounded", "shadow-md", "mb-4");
@@ -20,9 +20,16 @@ function createWeatherCard(temperature, description) {
     descriptionElement.textContent = `Description: ${description}`;
     descriptionElement.classList.add("text-white");
 
-    // Append temperature and description elements to the card
+    // Create weather icon element
+    const iconElement = document.createElement("img");
+    iconElement.src = `http://openweathermap.org/img/w/${icon}.png`; // Use the icon provided by OpenWeatherMap
+    iconElement.alt = "Weather Icon";
+    iconElement.classList.add("weather-icon");
+
+    // Append temperature, description, and icon elements to the card
     card.appendChild(temperatureElement);
     card.appendChild(descriptionElement);
+    card.appendChild(iconElement);
 
     return card;
 }
@@ -51,9 +58,10 @@ function getWeather() {
             const temperatureCelsius = Math.round(data.main.temp - 273.15); // Convert from Kelvin to Celsius
             const temperatureFahrenheit = convertToFarhenheit(temperatureCelsius);
             const description = data.weather[0].description;
+            const icon = data.weather[0].icon;
 
-            // Create a new weather card
-            const weatherCard = createWeatherCard(temperatureFahrenheit, description);
+            // Create a new weather card with icon
+            const weatherCard = createWeatherCard(temperatureFahrenheit, description, icon);
 
             // Clear previous content in weatherResult div
             weatherResult.innerHTML = "";
@@ -65,7 +73,7 @@ function getWeather() {
             console.error("Error fetching weather data:", error);
 
             // Display error message in a card
-            const errorCard = createWeatherCard("N/A", "Error fetching weather data");
+            const errorCard = createWeatherCard("N/A", "Error fetching weather data", "error");
             
             // Clear previous content in weatherResult div
             weatherResult.innerHTML = "";
